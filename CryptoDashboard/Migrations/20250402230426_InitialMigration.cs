feab8f6e89937b649a56acab8f69f6238b2690a2
Goruchie <marcosgualtero43253@gmail.com>
2025-04-02 20:17:40 -0300

    Generate the Initial Migration

---
+++ CryptoDashboard/Migrations/20250402230426_InitialMigration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CryptoCurrencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CryptoCurrencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CryptoPrices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CryptoCurrencyId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,8)", nullable: false),
+                    Volume = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CryptoPrices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CryptoPrices_CryptoCurrencies_CryptoCurrencyId",
+                        column: x => x.CryptoCurrencyId,
+                        principalTable: "CryptoCurrencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CryptoPrices_CryptoCurrencyId",
+                table: "CryptoPrices",
+                column: "CryptoCurrencyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CryptoPrices");
+
+            migrationBuilder.DropTable(
+                name: "CryptoCurrencies");
+        }
+    }
+}
