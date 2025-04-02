39ca1f9a82cab69d3ca683d39e18ce0d491b489e
Goruchie <marcosgualtero43253@gmail.com>
2025-04-02 18:16:29 -0300

    Create the CryptoCurrency & CryptoPrice Models

---
+++ CryptoDashboard/Models/CryptoCurrency.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoDashboard.Models
+{
+    public class CryptoCurrency
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+    }
+}
---
+++ CryptoDashboard/Models/CryptoPrice.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoDashboard.Models
+{
+    public class CryptoPrice
+    {
+        public int Id { get; set; }
+        public int CryptoCurrencyId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
