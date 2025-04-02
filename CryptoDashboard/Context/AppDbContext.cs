81232757b07ae4443c5787571d7ff93226032485
Goruchie <marcosgualtero43253@gmail.com>
2025-04-02 19:27:12 -0300

    Set up DbContext and database configurations

---
+++ CryptoDashboard/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CryptoDashboard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoDashboard.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<CryptoCurrency> CryptoCurrencies { get; set; }
+        public DbSet<CryptoPrice> CryptoPrices { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CryptoPrice>()
+                .HasOne<CryptoCurrency>()
+                .WithMany()
+                .HasForeignKey(p => p.CryptoCurrencyId);
+        }
+
+    }
+}
