6640f982b86539d423b5b06dd5763bfa6be96a04
Goruchie <marcosgualtero43253@gmail.com>
2025-04-02 17:25:49 -0300

    Create CryptoCurrencies Controller and integrate CoinGecko API

---
+++ CryptoDashboard/Controllers/CryptoCurrenciesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoDashboard.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoCurrenciesController : ControllerBase
+    {
+        [HttpGet("prices")]
+        public async Task<IActionResult> GetCryptoPrices()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return Ok(content);
+                }
+                return StatusCode((int)response.StatusCode, "Error fetching data from CoinGecko.");
+            }
+        }
+
+    }
+}
