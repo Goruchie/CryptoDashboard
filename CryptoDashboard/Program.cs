f68a327c2d258e8c2a14e67235233653f25e76d6
Goruchie <marcosgualtero43253@gmail.com>
2025-04-02 19:49:08 -0300

    Add database connection setup in appsettings.json and prepare DbContext for SQL Server

--- CryptoDashboard/Program.cs
+++ CryptoDashboard/Program.cs
@@ -1,6 +1,14 @@
+using CryptoDashboard.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
