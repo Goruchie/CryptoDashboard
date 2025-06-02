3d3c7ba69013fe7b64434302d654b251e8c8002f
Goruchie <marcosgualtero43253@gmail.com>
2025-06-01 23:17:45 -0300

    Disable Hangfire

--- CryptoDashboard/Program.cs
+++ CryptoDashboard/Program.cs
@@ -40,23 +40,23 @@ builder.Services.AddSwaggerGen();
 
 // Hangfire
 
-builder.Services.AddHangfire(config =>
-{
-    config.UsePostgreSqlStorage(connectionString, new Hangfire.PostgreSql.PostgreSqlStorageOptions
-    {
-        SchemaName = "HangFire",
-        QueuePollInterval = TimeSpan.FromMinutes(60),
-    });
-});
+//builder.Services.AddHangfire(config =>
+//{
+//    config.UsePostgreSqlStorage(connectionString, new Hangfire.PostgreSql.PostgreSqlStorageOptions
+//    {
+//        SchemaName = "HangFire",
+//        QueuePollInterval = TimeSpan.FromMinutes(60),
+//    });
+//});
 
-builder.Services.AddHangfireServer(options =>
-{
-    options.WorkerCount = 1; 
-    options.ShutdownTimeout = TimeSpan.FromMinutes(1); 
-});
+//builder.Services.AddHangfireServer(options =>
+//{
+//    options.WorkerCount = 1; 
+//    options.ShutdownTimeout = TimeSpan.FromMinutes(1); 
+//});
 
 
-builder.Services.AddScoped<CryptoJobService>();
+//builder.Services.AddScoped<CryptoJobService>();
 
 // 
 
@@ -91,18 +91,18 @@ app.MapControllers();
 //                                    });
 
 //Create hangfire job
-using (var scope = app.Services.CreateScope())
-{
-    var recurringJobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var recurringJobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
 
-    recurringJobManager.RemoveIfExists("FetchCryptoPrices"); 
+//    recurringJobManager.RemoveIfExists("FetchCryptoPrices"); 
 
-    recurringJobManager.AddOrUpdate(
-        "FetchCryptoPrices",
-        () => scope.ServiceProvider.GetRequiredService<CryptoJobService>().FetchAndStorePrices(),
-        "0 0 * * *" 
-    );
-}
+//    recurringJobManager.AddOrUpdate(
+//        "FetchCryptoPrices",
+//        () => scope.ServiceProvider.GetRequiredService<CryptoJobService>().FetchAndStorePrices(),
+//        "0 0 * * *" 
+//    );
+//}
 
 
 app.Run();
